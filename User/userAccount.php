c3f2b322494bc27e62d82226fdc7915b7373b6e0
yujie-liu <yliu134@u.rochester.edu>
2018-12-14 00:03:46 -0500

    Add login

---
+++ User/userAccount.php
@@ -0,0 +1,144 @@
+<?php
+//start session
+session_start();
+//load and initialize user class
+include 'user.php';
+$user = new User();
+if(isset($_POST['signupSubmit'])){
+    //check whether user details are empty
+    if(!empty($_POST['first_name']) && !empty($_POST['last_name']) && !empty($_POST['email']) && !empty($_POST['phone']) && !empty($_POST['password']) && !empty($_POST['confirm_password'])){
+        //password and confirm password comparison
+        if($_POST['password'] !== $_POST['confirm_password']){
+            $sessData['status']['type'] = 'error';
+            $sessData['status']['msg'] = 'Confirm password must match with the password.';
+        }else{
+            //check whether user exists in the database
+            $prevCon['where'] = array('email'=>$_POST['email']);
+            $prevCon['return_type'] = 'count';
+            $prevUser = $user->getRows($prevCon);
+            if($prevUser > 0){
+                $sessData['status']['type'] = 'error';
+                $sessData['status']['msg'] = 'Email already exists, please use another email.';
+            }else{
+                //insert user data in the database
+                $userData = array(
+                    'first_name' => $_POST['first_name'],
+                    'last_name' => $_POST['last_name'],
+                    'email' => $_POST['email'],
+                    'password' => md5($_POST['password']),
+                    'phone' => $_POST['phone']
+                );
+                $insert = $user->insert($userData);
+                //set status based on data insert
+                if($insert){
+                    $sessData['status']['type'] = 'success';
+                    $sessData['status']['msg'] = 'You have registered successfully, log in with your credentials.';
+                }else{
+                    $sessData['status']['type'] = 'error';
+                    $sessData['status']['msg'] = 'Some problem occurred, please try again.';
+                }
+            }
+        }
+    }else{
+        $sessData['status']['type'] = 'error';
+        $sessData['status']['msg'] = 'All fields are mandatory, please fill all the fields.';
+    }
+    //store signup status into the session
+    $_SESSION['sessData'] = $sessData;
+    $redirectURL = ($sessData['status']['type'] == 'success')?'index.php':'registration.php';
+    //redirect to the home/registration page
+    header("Location:".$redirectURL);
+}elseif(isset($_POST['loginSubmit'])){
+    //check whether login details are empty
+    if(!empty($_POST['email']) && !empty($_POST['password'])){
+    	 //get user data from user class
+        $conditions['where'] = array(
+            'email' => $_POST['email'],
+            'password' => md5($_POST['password']),
+            'status' => '1'
+        );
+        $conditions['return_type'] = 'single';
+        $userData = $user->getRows($conditions);
+        //set user data and status based on login credentials
+        if($userData){
+            $sessData['userLoggedIn'] = TRUE;
+            $sessData['userID'] = $userData['id'];
+            $sessData['status']['type'] = 'success';
+            $sessData['status']['msg'] = 'Welcome '.$userData['first_name'].'!';
+        }else{
+            $sessData['status']['type'] = 'error';
+            $sessData['status']['msg'] = 'Wrong email or password, please try again.';
+        }
+    }else{
+        $sessData['status']['type'] = 'error';
+        $sessData['status']['msg'] = 'Enter email and password.';
+    }
+    //store login status into the session
+    $_SESSION['sessData'] = $sessData;
+    //redirect to the home page
+    header("Location:index.php");
+}elseif(!empty($_REQUEST['logoutSubmit'])){
+    //remove session data
+    unset($_SESSION['sessData']);
+    session_destroy();
+    //store logout status into the ession
+    $sessData['status']['type'] = 'success';
+    $sessData['status']['msg'] = 'You have logout successfully from your account.';
+    $_SESSION['sessData'] = $sessData;
+    //redirect to the home page
+    header("Location:index.php");
+}else{
+    //redirect to the home page
+    header("Location:index.php");
+}
+Login Form and User Account Details (index.php)
+Initially index.php file is loaded with login form and registration page link. After login, the user ID is available in session and the respective user details are shown using the User class. Also, a logout link will appear if the user already logged in.
+
+<?php
+session_start();
+$sessData = !empty($_SESSION['sessData'])?$_SESSION['sessData']:'';
+if(!empty($sessData['status']['msg'])){
+    $statusMsg = $sessData['status']['msg'];
+    $statusMsgType = $sessData['status']['type'];
+    unset($_SESSION['sessData']['status']);
+}
+?>
+<style>
+    <?php
+         include 'style.css';
+    ?>
+</style>
+
+<div class="container">
+    <?php
+        if(!empty($sessData['userLoggedIn']) && !empty($sessData['userID'])){
+            include 'user.php';
+            $user = new User();
+            $conditions['where'] = array(
+                'id' => $sessData['userID'],
+            );
+            $conditions['return_type'] = 'single';
+            $userData = $user->getRows($conditions);
+    ?>
+    <h2>Welcome <?php echo $userData['first_name']; ?>!</h2>
+    <a href="userAccount.php?logoutSubmit=1" class="logout">Logout</a>
+    <div class="regisFrm">
+        <p><b>Name: </b><?php echo $userData['first_name'].' '.$userData['last_name']; ?></p>
+        <p><b>Email: </b><?php echo $userData['email']; ?></p>
+        <p><b>Phone: </b><?php echo $userData['phone']; ?></p>
+    </div>
+    <?php }else{ ?>
+    <h2>Login to Your Account</h2>
+    <?php echo !empty($statusMsg)?'<p class="'.$statusMsgType.'">'.$statusMsg.'</p>':''; ?>
+    <div class="regisFrm">
+        <form action="userAccount.php" method="post">
+            <input type="email" name="email" placeholder="EMAIL" required="">
+            <input type="password" name="password" placeholder="PASSWORD" required="">
+            <div class="send-button">
+                <input type="submit" name="loginSubmit" value="LOGIN">
+            </div>
+        </form>
+        <p>New User?<a href="registration.php">Register</a></p>
+    </div>
+    <?php } ?>
+</div>

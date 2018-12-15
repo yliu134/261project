2c705e1cb9ed2f9be511fd47babc6339c8a2fafb
yujie-liu <yliu134@u.rochester.edu>
2018-12-14 22:41:01 -0500

    update user

--- User/userAccount.php
+++ User/userAccount.php
@@ -5,8 +5,7 @@ session_start();
 include 'user.php';
 $user = new User();
 if(isset($_POST['signupSubmit'])){
-    //check whether user details are empty
-    if(!empty($_POST['Password']) && !empty($_POST['Pnum']) && !empty($_POST['Username']) && !empty($_POST['Confirm_password'])){
+if(!empty($_POST['Password']) && !empty($_POST['Pnum']) && !empty($_POST['Username']) && !empty($_POST['Confirm_password'])){
         //password and confirm password comparison
         if($_POST['Password'] !== $_POST['Confirm_password']){
             $sessData['status']['type'] = 'error';
@@ -15,12 +14,12 @@ if(isset($_POST['signupSubmit'])){
 
             //insert user data in the database
             $userData = array(
-				'Password' => $_POST['Password'],
+                'Password' => $_POST['Password'],
                 'Username' => $_POST['Username'],
                 'Pnum' => $_POST['Pnum']
             );
             $insert = $user->insert($userData);
-			$userData['CID'] = mysql_insert_id();
+            $userData['CID'] = mysql_insert_id();
             //set status based on data insert
             if($insert){
                 $sessData['status']['type'] = 'success';
@@ -37,21 +36,22 @@ if(isset($_POST['signupSubmit'])){
     }
     //store signup status into the session
     $_SESSION['sessData'] = $sessData;
-    $redirectURL = ($sessData['status']['type'] == 'success')?'index.php':'registration.php';
+    $redirectURL = ($sessData['status']['type'] == 'success')?'../index.php':'registration.php';
     //redirect to the home/registration page
     header("Location:".$redirectURL);
 }elseif(isset($_POST['loginSubmit'])){
-    //check whether login details are empty
     if(!empty($_POST['CID']) && !empty($_POST['Password'])){
-    	 //get user data from user class
-        $conditions['where'] = array(
+    echo "1";   
+    var_dump($_POST);
+
+    $conditions['where'] = array(
             'CID' => $_POST['CID'],
             'Password' => md5($_POST['Password'])
         );
         $conditions['return_type'] = 'single';
         $userData = $user->getRows($conditions);
-        //set user data and status based on login credentials
-        if($userData){
+    var_dump($userData);
+    if($userData){
             $sessData['userLoggedIn'] = TRUE;
             $sessData['CID'] = $userData['CID'];
             $sessData['status']['type'] = 'success';
@@ -67,7 +67,7 @@ if(isset($_POST['signupSubmit'])){
     //store login status into the session
     $_SESSION['sessData'] = $sessData;
     //redirect to the home page
-    header("Location:index.php");
+    header("Location:../index.php");
 }elseif(!empty($_REQUEST['logoutSubmit'])){
     //remove session data
     unset($_SESSION['sessData']);
@@ -82,6 +82,3 @@ if(isset($_POST['signupSubmit'])){
     //redirect to the home page
     header("Location:index.php");
 }
-Login Form and User Account Details (index.php)
-Initially index.php file is loaded with login form and registration page link. After login, the user ID is available in session and the respective user details are shown using the User class. Also, a logout link will appear if the user already logged in.
-

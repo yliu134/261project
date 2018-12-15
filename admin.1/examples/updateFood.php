75c810cc83af9d4355e723921522cf8abf5a7433
Xiaoya Wang <mia@vpn13-55.net.rochester.edu>
2018-12-15 04:18:40 -0500

    update

--- admin.1/examples/updateFood.php
+++ admin.1/examples/updateFood.php
@@ -75,13 +75,13 @@
           <li class="active ">
             <a href="./updateFood.php">
               <i class="tim-icons icon-bag-16"></i>
-              <p>Update food</p>
+              <p>Update/Delete food</p>
             </a>
           </li>
           <li>
             <a href="./updateDelivery.php">
               <i class="tim-icons icon-bus-front-12"></i>
-              <p>Update Delivery group</p>
+              <p>Update/Delete Delivery group</p>
             </a>
           </li>
         </ul>

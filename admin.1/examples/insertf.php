7723df7bbbdddd022fb07c1441bf72828a459a91
Xiaoya Wang <mia@vpn13-55.net.rochester.edu>
2018-12-15 00:31:47 -0500

    add f9

--- admin.1/examples/insertf.php
+++ admin.1/examples/insertf.php
@@ -18,7 +18,7 @@
   echo "Error using database: " . $conn->error;
   }
 
-$sql2="INSERT INTO FOOD (Fname, Fprice) VALUES ('huh',111)";
+$sql2="INSERT INTO FOOD (Fname, Fprice) VALUES (mysql_real_escape_string($_POST('fname')),111)";
 
 
 

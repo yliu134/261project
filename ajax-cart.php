7581dd649125efa3a5ba4815b0c529d09d75c225
Dengpan666 <Eason.d.pan@gmail.com>
2018-12-15 18:58:18 -0500

    submit 5

--- ajax-cart.php
+++ ajax-cart.php
@@ -122,7 +122,7 @@ switch ($_POST['req']) {
   //   break;
 
       case "checkout":      
-    if ()) {
+    if ($cartLib->oAdd("test", "test2")) {
       $_SESSION['cart'] = array();
       echo "OK";
     } else {

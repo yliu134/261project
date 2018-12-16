86748c3776bb0019490b9d6405e90db9cad03318
Xiaoya Wang <mia@vpn13-9.net.rochester.edu>
2018-12-15 23:07:09 -0500

    pic

--- admin.1/examples/order.php
+++ admin.1/examples/order.php
@@ -275,7 +275,9 @@ session_write_close();
                               $sql3 = "SELECT Fname from FOOD WHERE FID=". $row2["FID"];
                               $result3 = $conn->query($sql3);
                               if($result3 -> num_rows > 0){
-                                 $m .= $row2["Fname"];
+                                while ($row3 = $result3 -> fetch_assoc()){
+                                  $m .= $row3["Fname"];
+                                }
                               }else{
                                 $m .= "N/A";
                               }

e91539d8346818657cd79765b5019d2f98d2b05e
yujie-liu <yliu134@u.rochester.edu>
2018-12-15 19:19:59 -0500

    checkout 14

--- ajax-cart.php
+++ ajax-cart.php
@@ -122,12 +122,12 @@ switch ($_POST['req']) {
   //   break;
 
       case "checkout":      
-    // if ($cartLib->oAdd("test", "test2")) {
-    //   $_SESSION['cart'] = array();
+     if ($cartLib->oAdd("test", "test2")) {
+       $_SESSION['cart'] = array();
        echo "OKok";
-    // } else {
-    //   echo $cartLib->error;
-    // }
+     } else {
+       echo $cartLib->error;
+     }
     break;
 }
 ?>
